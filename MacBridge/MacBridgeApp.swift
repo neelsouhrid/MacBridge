7af737c20bee87bed8d5ac66098cb9735801d0fc
neelsouhrid <neelsouhrid@gmail.com>
2026-05-18 10:10:48 +0530

    Initial Commit

---
+++ MacBridge/MacBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacBridgeApp.swift
+//  MacBridge
+//
+//  Created by Souhrid Pal on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MacBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
